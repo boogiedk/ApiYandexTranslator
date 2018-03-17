5be069b9874530891461701417532b41c843d3de
boogiedk <ganz161@yandex.ru>
2018-03-17 12:23:34 +0300

    test

--- Translator/MainWindow.xaml.cs
+++ Translator/MainWindow.xaml.cs
@@ -19,129 +19,63 @@ using System.Windows.Interop;
 using System.Text.RegularExpressions;
 
 namespace Translator
-	{
-	/// <summary>
-	/// Логика взаимодействия для MainWindow.xaml
-	/// </summary>
-	public partial class MainWindow : Window
-		{
-		ApiYandex obj;
-		public MainWindow()
-			{
-			InitializeComponent();
-			obj = new ApiYandex();
-			Loaded += MainWindow_Loaded;
-			}
+{
+    public partial class MainWindow : Window
+    {
+        private ApiYandex connectApiYandex;
 
-		string lang = "en-ru";
-		int count = 0;
-		string buffer_str = "";
-		string buffer_str_trns = "";
-		string inTextEnter = "";
-		bool turn = false;
+        private string lang;
 
-		private void inText_TextChanged(object sender, TextChangedEventArgs e)
-			{
-			outText.Text = obj.Translate(inText.Text, lang);
-			count = inText.Text.Length;
-			countlabel.Content = count;
-			IdentifyEnterLang();
-			}
 
-		private void ShowStandardBalloon()
-			{
-			TaskbarIcon MyNotifyIcon = new TaskbarIcon();
-			TranslateStringFromBuffer();
+        public MainWindow()
+        {
+            InitializeComponent();
+            connectApiYandex = new ApiYandex();
+            Loaded += MainWindow_Loaded;
+            lang = "en-ru";
+        }
 
-			if (buffer_str != "" && buffer_str != null && turn)
-				{
-				MyNotifyIcon.ShowBalloonTip(buffer_str, buffer_str_trns, BalloonIcon.Info);
-				}
-			}
+        private void inText_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            int count = 0;
+            outText.Text = connectApiYandex.Translate(inText.Text, lang);
+            count = inText.Text.Length;
+            countlabel.Content = count;
+            IdentifyEnterLang();
+        }
 
-		private void GetStringFromBuffer()
-			{
-				{
-				if (turn)
-					{
-					buffer_str = Clipboard.GetText();
-					}
-				else
-					buffer_str = "";
-				}
-			}
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            var hwnd = new WindowInteropHelper(this).Handle;
+            var source = HwndSource.FromHwnd(hwnd);
 
-		private void TranslateStringFromBuffer()
-			{
-			GetStringFromBuffer();
-			if (buffer_str != "" && buffer_str != null)
-				{
-				buffer_str_trns = obj.Translate(buffer_str, lang);
-				}
-			}
+            source.AddHook(WndProc);
+            Win32.AddClipboardFormatListener(hwnd);
+        }
 
-		private void TurnBuffTranslate(object sender, RoutedEventArgs e)
-			{
-			if (turn)
-				{
-				ClickBuff.Header = "Включить буффер-перевод";
-				turn = false;
-				}
-			else
-				{
-				ClickBuff.Header = "Выключить буффер-перевод";
-				turn = true;
-				}
-			}
+        private IntPtr WndProc(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+        {
+            if (msg == Win32.WM_CLIPBOARDUPDATE)
+            {
+                // method for ballon show
+            }
 
-		private void MainWindow_Loaded(object sender, RoutedEventArgs e)
-			{
-			var hwnd = new WindowInteropHelper(this).Handle;
-			var source = HwndSource.FromHwnd(hwnd);
+            return IntPtr.Zero;
+        }
 
-			source.AddHook(WndProc);
-			Win32.AddClipboardFormatListener(hwnd);
-			}
-
-		private IntPtr WndProc(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
-			{
-			if (msg == Win32.WM_CLIPBOARDUPDATE)
-				{
-				ShowStandardBalloon();
-				}
-
-			return IntPtr.Zero;
-			}
-
-		private void MenuItemSetting_Click(object sender, RoutedEventArgs e)
-			{
-			SettingPage obj_open = new SettingPage();
-			obj_open.Show();
-			}
-
-		private void IdentifyEnterLang()
-			{
-			string inTextEnter = inText.Text;
-			if (Regex.IsMatch(inTextEnter, "^[A-Za-z' ']+$") == true)
-				{
-				lang = "en-ru";
-				langlabel.Content = "Английский > Русский";
-				}
-			else
-				{
-				lang = "ru-en";
-				langlabel.Content = "Русский > Английский";
-				}
-			}
-
-		private void MenuItem_Click(object sender, RoutedEventArgs e)
-			{
-
-			}
-
-		private void AboutApplication(object sender, RoutedEventArgs e)
-			{
-			MessageBox.Show("BOOGIE INC 2017 (c)");
-			}
-		}
-	}
+        private void IdentifyEnterLang()
+        {
+            string inTextEnter = inText.Text;
+            if (Regex.IsMatch(inTextEnter, "^[A-Za-z' ']+$") == true)
+            {
+                lang = "en-ru";
+                langlabel.Content = "Английский > Русский";
+            }
+            else
+            {
+                lang = "ru-en";
+                langlabel.Content = "Русский > Английский";
+            }
+        }
+    }
+}
---
+++ Translator/WebModul/ApiYandex.cs
@@ -0,0 +1,51 @@
+﻿using System.Net;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace Translator
+{
+    class ApiYandex
+    {
+        public string Translate(string s, string lang)
+        {
+            const string
+                apiKey =
+                    "trnsl.1.1.20170809T111737Z.11cf930211c4be06.71600bfdff1e34a5d9f647ddcd774bcd7de4f1fe"; // you need insert your api=key
+
+            if (s.Length > 0)
+            {
+                var request = WebRequest.Create("https://translate.yandex.net/api/v1.5/tr.json/translate?"
+                                                + "key=" + apiKey + "&text=" + s + "&lang=" + lang);
+
+                var response = request.GetResponse();
+
+                using (var stream = new StreamReader(response.GetResponseStream()))
+                {
+                    string line;
+
+                    if ((line = stream.ReadLine()) != null)
+                    {
+                        var tranlation = JsonConvert.DeserializeObject<Translation>(line);
+                        s = "";
+
+                        foreach (string str in tranlation.Text)
+                        {
+                            s += str;
+                        }
+                    }
+                }
+
+                return s;
+            }
+            else
+                return "";
+        }
+
+        class Translation
+        {
+            public string Code { get; set; }
+            public string Lang { get; set; }
+            public string[] Text { get; set; }
+        }
+    }
+}
